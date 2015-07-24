7ca39a5c643ea26e23dfba389cbd0392b232b965
Jorge Falcon <jfalcon95@outlook.com>
2015-07-24 01:03:03 -0500

    prueba4

--- js/fblogin.js
+++ js/fblogin.js
@@ -71,7 +71,7 @@
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
+      console.log('Successful login for: ' + response);
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
     });
