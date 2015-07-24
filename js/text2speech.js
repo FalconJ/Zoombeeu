f9d65be3a00c3b4029a4ec9c9427eda99d4b93da
Jorge Falcon <jfalcon95@outlook.com>
2015-07-24 09:01:58 -0500

    prueba text2speech

--- js/text2speech.js
+++ js/text2speech.js
@@ -23,6 +23,11 @@ $('#contrasena').mouseenter(function() { // Attach this function to all mouseent
   responsiveVoice.speak("Ingrese su contraseña"); // Speak the text contents of all nodes within the current 'a' tag
 });
 
+$('a, h2, button')on('swipe', function(){
+  responsiveVoice.cancel(); // Cancel anything else that may currently be speaking
+  responsiveVoice.speak($(this).text()); // Speak the text contents of all nodes within the current 'a' tag
+});
+
 $(document).ready(function(){
 	responsiveVoice.setDefaultVoice("Spanish Latin American Female");
 });
