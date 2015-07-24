9afc6cfa671b4b4b47a60a76c00b8c6d3602b6ca
Jorge Falcon <jfalcon95@outlook.com>
2015-07-24 06:57:14 -0500

    text2speech para landing

--- js/text2speech.js
+++ js/text2speech.js
@@ -1,12 +1,26 @@
-$('a').mouseenter(function() { // Attach this function to all mouseenter events for 'a' tags 
+$('a, h2, button').mouseenter(function() { // Attach this function to all mouseenter events for 'a' tags 
   responsiveVoice.cancel(); // Cancel anything else that may currently be speaking
   responsiveVoice.speak($(this).text()); // Speak the text contents of all nodes within the current 'a' tag
 });
 
 $('#loginBtn').mouseenter(function() { // Attach this function to all mouseenter events for 'a' tags 
   responsiveVoice.cancel(); // Cancel anything else that may currently be speaking
-  responsiveVoice.speak("¡Ingresa a la comunidad!"); // Speak the text contents of all nodes within the current 'a' tag
-  console.log("¡Ingresa a la comunidad!");
+  responsiveVoice.speak("Iniciar sesión con Facebook"); // Speak the text contents of all nodes within the current 'a' tag
+});
+
+$('#logo').mouseenter(function() { // Attach this function to all mouseenter events for 'a' tags 
+  responsiveVoice.cancel(); // Cancel anything else that may currently be speaking
+  responsiveVoice.speak("Zoombeeu", "UK English Female"); // Speak the text contents of all nodes within the current 'a' tag
+});
+
+$('#correo').mouseenter(function() { // Attach this function to all mouseenter events for 'a' tags 
+  responsiveVoice.cancel(); // Cancel anything else that may currently be speaking
+  responsiveVoice.speak("Ingrese su correo electrónico"); // Speak the text contents of all nodes within the current 'a' tag
+});
+
+$('#contrasena').mouseenter(function() { // Attach this function to all mouseenter events for 'a' tags 
+  responsiveVoice.cancel(); // Cancel anything else that may currently be speaking
+  responsiveVoice.speak("Ingrese su contraseña"); // Speak the text contents of all nodes within the current 'a' tag
 });
 
 $(document).ready(function(){
