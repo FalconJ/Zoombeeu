13e25619f40df33eb8dad4387bbc1f5223c4b47d
Jorge Falcon <jfalcon95@outlook.com>
2015-07-24 03:02:42 -0500

    Array de la info usuarios

--- js/fblogin.js
+++ js/fblogin.js
@@ -69,14 +69,6 @@
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-
-      console.log('Successful login for: ' + response);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-    });
-
     FB.api('/me', {fields: 'id,name,email,first_name,last_name,gender'}, function(response) {
       console.log(response);
       
